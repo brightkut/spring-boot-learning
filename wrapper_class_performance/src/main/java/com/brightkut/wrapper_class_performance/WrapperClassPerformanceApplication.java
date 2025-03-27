91228e749903c32ff0efb96a30d46c5479461521
brightkut <dsorn2@gmail.com>
2025-03-27 07:43:36 +0700

    feat: add module wrapper and custom annotation

---
+++ wrapper_class_performance/src/main/java/com/brightkut/wrapper_class_performance/WrapperClassPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.brightkut.wrapper_class_performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapperClassPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrapperClassPerformanceApplication.class, args);
+    }
+
+}
